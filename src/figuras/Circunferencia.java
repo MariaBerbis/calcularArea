9a05b498dad474bad6c7cf6ff19b51a56deb6d7a
MariaBerbis <maria.berbis.m@gmail.com>
2025-01-09 22:24:42 +0000

    Changes asked in the exercise

--- src/figuras/Circunferencia.java
+++ src/figuras/Circunferencia.java
@@ -1,36 +1,30 @@
-package utilidades;
+package figuras;
 
-public class Circulo {
+public class Circunferencia {
     private final double rad;
 
-    public Circulo(double radio)
-    {
+    public Circunferencia(double radio) {
         this.rad = radio;
     }
 
-    public void imprimir()
-    {
+    public void imprimir() {
         String color = "rojo";
-        System.out.println("Diametro: " + 2*rad);
+        System.out.println("Diametro: " + 2 * rad);
         System.out.println("Color: " + color);
         double area = 2 * 3.1416 * rad * rad;
         System.out.println(area);
     }
 
-    public boolean esIgual (Circulo otro, boolean conDecimales)
-    {
+    public boolean esIgual(Circunferencia otro, boolean conDecimales) {
         double radio1 = this.rad;
         double radio2 = otro.getRad();
-        if (conDecimales)
-        {
+        if (conDecimales) {
             if (radio1 == radio2)
                 return true;
             else
                 return false;
-        }
-        else
-        {
-            if (Math.abs(radio1-radio2)<1)
+        } else {
+            if (Math.abs(radio1 - radio2) < 1)
                 return true;
             else
                 return false;
